7d5f2925bccbe939e7e795935909e085484acb66
Evan Ho <77393541+ehackerevan@users.noreply.github.com>
2026-08-29 11:55:13 +0800

    Cache Decision OS assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='stoic30-v3';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./neutral-v3.js'];
+const CACHE='stoic30-v4-decision-os';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./neutral-v3.js','./decision-os.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
